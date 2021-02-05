d4a470197a94868f63ca5e45a13eaa209b0705a0
Serge Simplice Kenne <kenne_serge@mail.ru>
2021-02-05 12:38:58 +0300

    add loading spinner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
 
 const App = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true);
+        setTimeout(()=>{
+            setLoading(false)
+        }, 8000)
+    },[])
     return (
         <div className="app">
-            <Navbar/>
-            <Main/>
-            <Footer/>
+            {loading ? (
+                <div className="load-spinner">
+                    <ClimbingBoxLoader color={'#183b5e'} loading={loading} size={15} />
+                </div>
+            ) : (
+                <>
+                    <Navbar />
+                    <Main />
+                    <Footer />
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default App
