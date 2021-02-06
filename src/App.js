2cf348cc5810c1b604e621764be469fa5629a598
Serge Simplice Kenne <kenne_serge@mail.ru>
2021-02-06 04:51:30 +0300

    change spinner loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
+import GridLoader from 'react-spinners/GridLoader';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
 
 const App = () => {
 
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
-        setLoading(true);
         setTimeout(()=>{
             setLoading(false)
-        }, 8000)
+        }, 5000)
     },[])
     return (
         <div className="app">
             {loading ? (
                 <div className="load-spinner">
-                    <ClimbingBoxLoader color={'#183b5e'} loading={loading} size={15} />
+                    <GridLoader color={'#183b5e'} loading={loading} size={20} />
                 </div>
             ) : (
                 <>
